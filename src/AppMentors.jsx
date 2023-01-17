e8b26d501be166036035951cda3503e2dafeec94
eunbiggabi <eunbiggabi84@gmail.com>
2023-01-17 14:45:14 +1000

    understanding of update, add, and delete of react

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -2,25 +2,29 @@ import React, { useState } from 'react';
 
 export default function AppMentors() {
   const [person, setPerson] = useState(initialPerson);
+  
   const handleUpdate = () => {
-    const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
-    const current = prompt(`직함을 무엇으로 바꾸고 싶은가요?`);
-    setPerson(person => ({...person, mentors: person.mentors.map((mentor) => {
+    const prev = prompt("who's mentor's name you want to change")
+    const current = prompt("what's name you want to change")
+    setPerson(person => ({...person, mentors: person.mentors.map(mentor => {
       if (mentor.name === prev) {
-        return {...mentor, name: current};
+        return {...mentor, name: current}
       }
       return mentor;
     })}))
   }
+
   const handleAdd = () => {
-    const name = prompt(`어떤 멘토를 추가하고 싶은가요?`);
-    const title = prompt(`멘토의 직함은 무엇인가요?`);
+    const name = prompt("who you want it to add");
+    const title = prompt("what's title..")
     setPerson(person => ({...person, mentors: [...person.mentors, {name, title}]}))
   }
 
   const handleDelete = () => {
-    const name = prompt(`삭제할 멘토는 누구인가요?`);
-    setPerson(person => ({...person, mentors: person.mentors.filter((mentor) => mentor.name !== name)}))
+    const name = prompt("who you want it to delete");
+    setPerson(person => ({...person, mentors: person.mentors.filter(mentor => 
+      mentor.name !== name
+    )}))
   }
 
   return (
